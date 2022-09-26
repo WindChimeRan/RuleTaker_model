4a1d01a1f0b28221acf2ac4f2117b1cd853e0625
Ranran <1012869439@qq.com>
2022-09-26 05:24:24 -0400

    pl dataloader ok

--- dataloader.py
+++ dataloader.py
@@ -4,38 +4,26 @@ from torch.utils.data import Dataset, DataLoader
 
 import pytorch_lightning as pl
 
-# from pytorch_lightning.metrics.functional import accuracy, f1, auroc
-# from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
-# from pytorch_lightning.loggers import TensorBoardLogger
-
 
 class RuleTakerDataModule(pl.LightningDataModule):
-    def __init__(
-        self, train_path, test_path, tokenizer, batch_size=8, max_token_len=128
-    ):
+    def __init__(self, train_path, test_path, pretrained_model, batch_size=8):
         super().__init__()
         self.batch_size = batch_size
-        self.train_df = train_df
-        self.test_df = test_df
-        self.tokenizer = tokenizer
-        self.max_token_len = max_token_len
+        self.train_path = train_path
+        self.test_path = test_path
+        self.pretrained_model = pretrained_model
 
     def setup(self, stage=None):
-        self.train_dataset = RuleTakerDataset(
-            self.train_df, self.tokenizer, self.max_token_len
-        )
-
-        self.test_dataset = RuleTakerDataset(
-            self.test_df, self.tokenizer, self.max_token_len
-        )
+        self.train_dataset = RuleTakerDataset(self.pretrained_model, self.train_path)
+        self.test_dataset = RuleTakerDataset(self.pretrained_model, self.test_path)
 
     def train_dataloader(self):
         return DataLoader(
-            self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=2
+            self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4
         )
 
     def val_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=2)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=4)
 
     def test_dataloader(self):
-        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=2)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=4)
