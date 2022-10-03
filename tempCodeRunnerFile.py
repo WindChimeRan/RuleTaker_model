68104919f3d91ee506e88773db0b29ee5957edb3
Ranran <1012869439@qq.com>
2022-10-03 18:53:24 -0400

    rm trash

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# elif self._syntax == "propositional-meta":
-#     text = question[1]["question"]
-#     q_id = f"{item_id}-{question[0]}"
-#     label = question[1].get("propAnswer")
-#     if label is not None:
-#         label = ["False", "True", "Unknown"].index(label)
